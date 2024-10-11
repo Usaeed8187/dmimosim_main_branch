caa7e12fa637993a6d1a916fee992e6006c86c01
Donald Liang <yiliang@vt.edu>
2024-10-11 14:22:42 -0400

    Update previous NCJT simulation script
    * Make it run regardless of current working directory

--- sims/sim_ncjt.py
+++ sims/sim_ncjt.py
@@ -1,24 +1,22 @@
 """
 Simulation of NCJT scenario with ns-3 channels
 
-This scripts should be called from the "sims" folder
 """
 
 # add system folder for the dmimo library
 import os
 import sys
-
-current_dir = (os.getcwd())
-indx = current_dir.find('dmimosim')
-sys.path.append(current_dir[:indx + len('dmimosim')])
+import numpy as np
+# import matplotlib.pyplot as plt
 
 # Comment this part out if it doesn't matter which GPU to use:
 gpu_num = '0'  # Use "" to use the CPU, Use 0 to select first GPU
 os.environ["CUDA_VISIBLE_DEVICES"] = gpu_num
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import matplotlib.pyplot as plt
-import numpy as np
+# Add system path for the dmimo library
+dmimo_root = os.path.abspath(os.path.dirname(__file__) + "/..")
+sys.path.append(dmimo_root)
 
 from dmimo.config.ncjtsimconfig import NcjtSimConfig
 from dmimo.ncjt.ncjt_old import ncjt_sim_all_phases
@@ -32,7 +30,7 @@ if __name__ == "__main__":
     cfg.num_subframes = 9  # total number of subframes in ns-3 channels
     cfg.num_ofdm_symbols = 14  # number of OFDM symbols per subframe
     cfg.num_bits_per_symbol_phase2 = 2  # 2 for QPSK and 4 for 16QAM
-    cfg.ns3_folder = '../ns3/channels'
+    cfg.ns3_folder = os.path.join(dmimo_root, 'ns3/channels_medium_mobility')
     cfg.perSC_SNR = False
     # cfg.NOISE_FLOOR = -180  # uncomment if you want to have zero noise.
     cfg.num_bits_per_symbol_phase1 = 8
@@ -62,7 +60,7 @@ if __name__ == "__main__":
             pass
     BER_list = np.mean(BER_list, axis=-1)
 
-    folder = '../results/NCJT_Alamouti_QTR3/'
+    folder = os.path.join(dmimo_root, 'results/NCJT_Alamouti_QTR3/')
     child = 'QPSK/' if cfg.num_bits_per_symbol_phase2 == 2 else f'{2 ** cfg.num_bits_per_symbol_phase2}QAM/'
     child = child + ('ExactSNR/' if cfg.perSC_SNR else 'noSNR/')
     os.makedirs(folder + child, exist_ok=True)
