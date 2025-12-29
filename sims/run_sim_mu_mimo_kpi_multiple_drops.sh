f0f39e581d70e2ff25421d5eb28b06cf4c9a1aca
Usama <usaeed8187@vt.edu>
2025-12-29 12:33:52 -0500

    parallelizing simulation suns

--- sims/run_sim_mu_mimo_kpi_multiple_drops.sh
+++ sims/run_sim_mu_mimo_kpi_multiple_drops.sh
@@ -13,36 +13,41 @@ declare -a perfect_csi_arr=("False")
 declare -a channel_prediction_settings=("weiner_filter") # "None", "weiner_filter", "two_mode". If "None", cfg.csi_prediction = False. otherwise, cfg.csi_prediction = True and cfg.channel_prediction_method is changed accordingly.
 declare -a csi_quantization_arr=("True")
 
-# Loop through the arrays
-for i in ${!mobilities[@]}; do
-    for j in ${!drop_idx[@]}; do
-        for k in ${!rx_ues_arr[@]}; do
-            for m in ${!modulation_orders[@]}; do
-                for c in ${!code_rates[@]}; do
-                    for t in ${!num_txue_sel_arr[@]}; do
-                        for pcsi in ${!perfect_csi_arr[@]}; do
-                            for cp_setting in ${!channel_prediction_settings[@]}; do
-                                for cquant in ${!csi_quantization_arr[@]}; do
-                                    channel_prediction_setting=${channel_prediction_settings[$cp_setting]}
-                                    csi_prediction_enabled="False"
-                                    channel_prediction_method="None"
+PARALLEL_JOBS=${PARALLEL_JOBS:-4}
+PARALLEL_EXTRA_OPTS=${PARALLEL_EXTRA_OPTS:-}
 
-                                    if [[ "${channel_prediction_setting}" != "None" ]]; then
-                                        csi_prediction_enabled="True"
-                                        channel_prediction_method=${channel_prediction_setting}
-                                    fi
 
-                                    if [[ "${perfect_csi_arr[$pcsi]}" == "True" && "${csi_prediction_enabled}" == "True" ]]; then
-                                        continue
-                                    fi
-                                    if [[ "${perfect_csi_arr[$pcsi]}" == "False" && "${csi_quantization_arr[$cquant]}" == "False" ]]; then
-                                        continue
-                                    fi
-                                    if [[ "${csi_prediction_enabled}" == "True" && "${csi_quantization_arr[$cquant]}" == "False" ]]; then
-                                        continue
-                                    fi
-                                    echo "Mobility: ${mobilities[$i]}, Drop idx: ${drop_idx[$j]}, Rx UEs: ${rx_ues_arr[$k]}, Modulation order: ${modulation_orders[$m]}, Code rate: ${code_rates[$c]}, num_txue_sel: ${num_txue_sel_arr[$t]}, perfect_csi: ${perfect_csi_arr[$pcsi]}, channel_prediction_setting: ${channel_prediction_setting}, csi_prediction: ${csi_prediction_enabled}, csi_quantization_on: ${csi_quantization_arr[$cquant]}, channel_prediction_method: ${channel_prediction_method}"
-                                    python sims/sim_mu_mimo_testing_updates.py "${mobilities[$i]}" "${drop_idx[$j]}" "${rx_ues_arr[$k]}" "${modulation_orders[$m]}" "${code_rates[$c]}" "${num_txue_sel_arr[$t]}" "${perfect_csi_arr[$pcsi]}" "${channel_prediction_setting}" "${csi_quantization_arr[$cquant]}"                                    
+generate_args() {
+    # Loop through the arrays
+    for i in ${!mobilities[@]}; do
+        for j in ${!drop_idx[@]}; do
+            for k in ${!rx_ues_arr[@]}; do
+                for m in ${!modulation_orders[@]}; do
+                    for c in ${!code_rates[@]}; do
+                        for t in ${!num_txue_sel_arr[@]}; do
+                            for pcsi in ${!perfect_csi_arr[@]}; do
+                                for cp_setting in ${!channel_prediction_settings[@]}; do
+                                    for cquant in ${!csi_quantization_arr[@]}; do
+                                        channel_prediction_setting=${channel_prediction_settings[$cp_setting]}
+                                        csi_prediction_enabled="False"
+                                        channel_prediction_method="None"
+                                        if [[ "${channel_prediction_setting}" != "None" ]]; then
+                                            csi_prediction_enabled="True"
+                                            channel_prediction_method=${channel_prediction_setting}
+                                        fi
+
+                                        if [[ "${perfect_csi_arr[$pcsi]}" == "True" && "${csi_prediction_enabled}" == "True" ]]; then
+                                            continue
+                                        fi
+                                        if [[ "${perfect_csi_arr[$pcsi]}" == "False" && "${csi_quantization_arr[$cquant]}" == "False" ]]; then
+                                            continue
+                                        fi
+                                        if [[ "${csi_prediction_enabled}" == "True" && "${csi_quantization_arr[$cquant]}" == "False" ]]; then
+                                            continue
+                                        fi
+
+                                        echo "Mobility: ${mobilities[$i]}, Drop idx: ${drop_idx[$j]}, Rx UEs: ${rx_ues_arr[$k]}, Modulation order: ${modulation_orders[$m]}, Code rate: ${code_rates[$c]}, num_txue_sel: ${num_txue_sel_arr[$t]}, perfect_csi: ${perfect_csi_arr[$pcsi]}, channel_prediction_setting: ${channel_prediction_setting}, csi_prediction: ${csi_prediction_enabled}, csi_quantization_on: ${csi_quantization_arr[$cquant]}, channel_prediction_method: ${channel_prediction_method}" >&2
+                                        echo "${mobilities[$i]} ${drop_idx[$j]} ${rx_ues_arr[$k]} ${modulation_orders[$m]} ${code_rates[$c]} ${num_txue_sel_arr[$t]} ${perfect_csi_arr[$pcsi]} ${channel_prediction_setting} ${csi_quantization_arr[$cquant]}"
                                 done
                             done
                         done
@@ -51,8 +56,9 @@ for i in ${!mobilities[@]}; do
             done
         done
     done
-done
+}
 
+generate_args | parallel -j "${PARALLEL_JOBS}" ${PARALLEL_EXTRA_OPTS} --colsep ' ' python sims/sim_mu_mimo_testing_updates.py {1} {2} {3} {4} {5} {6} {7} {8} {9}
 
 # Reference table
 # Perfect CSI |  Prediction | Quantization | Meaning
