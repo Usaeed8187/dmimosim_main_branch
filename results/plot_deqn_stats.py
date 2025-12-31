e76a20ecfcaa11bffa22a1988512557a2bcb98a8
Usama <usaeed8187@vt.edu>
2025-12-31 14:34:33 -0500

    modifying plotting scripts

--- results/plot_deqn_stats.py
+++ results/plot_deqn_stats.py
@@ -282,7 +282,7 @@ def main() -> None:
         "--drops",
         type=int,
         nargs="+",
-        default=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+        default=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
         help="List of drop identifiers to include in the plots.",
     )
 
