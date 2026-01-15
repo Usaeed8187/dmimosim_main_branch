3c5d4e46096020dbc4d73aed4fdda66468ff5d26
Usama <usaeed8187@vt.edu>
2026-01-14 20:51:14 -0500

    bug fixes

---
+++ results/plot_rewards_and_throughput.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+base_dir = 'results/channels_multiple_mu_mimo/'
+ 
+mobility = 'high_mobility'
+
+method = 'deqn'
+
+rx_UEs = 4
+tx_UEs = 2
+
+drops = np.arange(1,90)
+
+rewards = []
+rl_throughput = []
+sl_throughput = []
+for drop_idx in drops:
+    rl_rewards_data = np.load('{}channels_{}_{}/{}_rewards_drop_{}_rx_UE_{}_tx_UE_{}_imitation_none_steps_0.npz'.format(base_dir, mobility, drop_idx,method,drop_idx,rx_UEs,tx_UEs))
+    rewards.append(rl_rewards_data["rewards"])
+
+    rl_tput_data = np.load('{}channels_{}_{}/mu_mimo_results_link_adapt_rx_UE_{}_tx_UE_{}_prediction_deqn_plus_two_mode_pmi_quantization_True_imitation_none_steps_0.npz'.format(base_dir, mobility, drop_idx, rx_UEs,tx_UEs))
+    rl_throughput.append(rl_tput_data['per_step_throughput'])
+
+    sl_tput_data = np.load('{}channels_{}_{}/mu_mimo_results_link_adapt_rx_UE_{}_tx_UE_{}_prediction_two_mode_pmi_quantization_True.npz'.format(base_dir, mobility, drop_idx, rx_UEs,tx_UEs))
+    sl_throughput.append(sl_tput_data['per_step_throughput'])
+
+    hold = 1
+
+rewards = np.concatenate(rewards)
+rl_throughput = np.concatenate(rl_throughput)
+sl_throughput = np.concatenate(sl_throughput)
+
+window_len = 100
+kernel = np.ones(window_len, dtype=float) / float(window_len)
+
+rewards = np.convolve(np.asarray(rewards, dtype=float), kernel, mode="valid")
+rl_throughput = np.convolve(np.asarray(rl_throughput, dtype=float), kernel, mode="valid")
+sl_throughput = np.convolve(np.asarray(sl_throughput, dtype=float), kernel, mode="valid")
+
+plt.figure()
+plt.plot(rewards)
+plt.savefig('aa')
+
+plt.figure()
+plt.plot(rl_throughput, label="DEQN + WESN")
+plt.plot(sl_throughput, label="WESN")
+plt.legend()
+plt.savefig('bb')
--- sims/run_sim_mu_mimo_kpi_multiple_drops.sh
+++ sims/run_sim_mu_mimo_kpi_multiple_drops.sh
@@ -4,7 +4,7 @@
 # declare -a mobilities=("low_mobility" "medium_mobility" "high_mobility")
 declare -a mobilities=("high_mobility")
 # declare -a drop_idx=("26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "43" "44" "45")
-declare -a drop_idx=($(seq 51 100))
+declare -a drop_idx=($(seq 1 100))
 declare -a rx_ues_arr=("4")
 declare -a num_txue_sel_arr=("2")
 declare -a modulation_orders=("2" "4")
