d367592e696407d17580c72ee0e4ec1a186421fe
Usama <usaeed8187@vt.edu>
2026-03-24 16:27:19 -0400

    adding kalman filtering

--- sims/sim_mu_mimo_testing_updates.py
+++ sims/sim_mu_mimo_testing_updates.py
@@ -94,9 +94,9 @@ code_rate = 1 / 2
 link_adapt = True
 
 perfect_csi = False
-channel_prediction_setting = "weiner_filter" # "None", "two_mode", "weiner_filter", "kalman_filter"
+channel_prediction_setting = "kalman_filter" # "None", "two_mode", "weiner_filter", "kalman_filter"
 csi_prediction = True
-channel_prediction_method = "weiner_filter" # None, "two_mode", "weiner_filter", "kalman_filter"
+channel_prediction_method = "kalman_filter" # None, "two_mode", "weiner_filter", "kalman_filter"
 csi_quantization_on = True
 rl_train_end_drop = 45
 rl_checkpoint = None
